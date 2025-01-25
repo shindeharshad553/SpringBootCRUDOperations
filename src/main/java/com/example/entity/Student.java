6af0421973660f1704627914d206efb9e06df9ba
shindeharshad553 <shindeharshad553@gmail.com>
2025-01-25 19:51:21 +0530

    updated student.java

--- src/main/java/com/example/entity/Student.java
+++ src/main/java/com/example/entity/Student.java
@@ -1,11 +1,14 @@
 package com.example.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class Student {
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
     private String name;
     private int age;
