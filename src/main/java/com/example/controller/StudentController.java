106ae210fee33fbcc1346457ec6f68a671b1b199
shindeharshad553 <shindeharshad553@gmail.com>
2025-01-25 19:51:42 +0530

    updated studentController.java

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -2,10 +2,11 @@ package com.example.controller;
 
 import com.example.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.example.entity.Student;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -17,5 +18,16 @@ public class StudentController {
     public String insertStudent(@RequestBody Student stud) {
         ss.insertStudent(stud);
         return "data inserted successfully";
+    }
+
+    @DeleteMapping("/deletedata/{stud_id}")
+    public String deleteStudent(@PathVariable int stud_id) {
+        ss.deleteStudent(stud_id);
+        return "data deleted successfully";
+    }
+
+    @PutMapping("/updatedata/{stud_id}")
+    public void updateStudent(@PathVariable int stud_id, @RequestBody Student s){
+        ss.updateStudent(stud_id,s);
     }
 }
